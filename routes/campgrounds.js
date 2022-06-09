27644824bd2eb3c6eb7e66931dfab791f643097c
artkalugin <artkalugin27@gmail.com>
2022-06-09 22:47:25 +0500

    change success flash message

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -54,7 +54,7 @@ router.get('/:id/edit', catchAsync(async (req, res) => {
 router.put('/:id', validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
-    req.flash('success', 'Successfully updated campground');
+    req.flash('success', 'Campground updated');
     res.redirect(`/campgrounds/${campground._id}`);
 }))
 
